2fabacf08bb71075da6ffca4f8b25e6f5169cdf0
lpeil <luan@peil.dev>
2020-11-10 19:47:30 -0300

    Fix deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
+  cssModules: true,
   cssLoaderOptions: {
-    importLoaders: true,
+    importLoaders: 1,
     localIdentName: '[local]',
+  },
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.+(js|jsx|ts|tsx)$/,
+      loader: defaultLoaders.babel,
+      include: /react-ui/,
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    });
+
+    return config;
   },
 });
