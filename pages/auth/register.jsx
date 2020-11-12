2018c7f7fcacb25192abb015fbdd797e3175eead
lpeil <luan@peil.dev>
2020-11-12 18:12:02 -0300

    Create user and register page

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -7,17 +7,19 @@ import { AuthCard } from '../../components';
 
 const Login = () => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    setError('Erro teste');
+    setError('teste');
   };
 
   return (
-    <AuthCard title="Entrar">
+    <AuthCard title="Cadastra-se">
       <form onSubmit={handleSubmit}>
         {error ? (
           <Alert className="alert" severity="error">
@@ -32,18 +34,32 @@ const Login = () => {
           onChange={(e) => setUsername(e.target.value)}
         />
         <TextField
+          name="email"
+          type="email"
+          value={email}
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
           name="password"
           type="password"
           value={password}
           label="Senha"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button variant="contained" color="primary" type="submit">
+        <TextField
+          name="password_confirmation"
+          type="password"
+          value={passwordConfirmation}
+          label="Repetir Senha"
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <Button variant="contained" color="primary">
           Entrar
         </Button>
       </form>
       <div className="links">
-        <Link href="register">Cadastrar-se</Link>
+        <Link href="login">Entrar</Link>
         <Link href="recoverPassword">Recuperar Senha</Link>
       </div>
     </AuthCard>
